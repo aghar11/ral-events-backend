50f14705007c445f9981f0bc0877d45c53511cfd
Akarsh Gharge <akarshgharge20@gmail.com>
2025-05-28 14:21:21 -0400

    Added UTC formatting for returned dates

--- app/routes/events.py
+++ app/routes/events.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from .. import crud, schemas
 from ..database import SessionLocal
+from datetime import timezone
 
 router = APIRouter()
 
@@ -16,4 +17,11 @@ def get_db():
 
 @router.get("/events", response_model=list[schemas.Event])
 def read_events(db: Session = Depends(get_db)):
-    return crud.get_events(db)
+    events = crud.get_events(db)
+
+    for event in events:
+        if event.startDate.tzinfo is None:
+            event.startDate = event.startDate.replace(tzinfo=timezone.utc)
+        if event.endDate.tzinfo is None:
+            event.endDate = event.endDate.replace(tzinfo=timezone.utc)
+    return events
